c425600515db035172cfd96b332bc908622719f3
Sebastian <jspaucar1@gmail.com>
2024-03-07 08:11:53 -0500

    suscribe

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TareasService } from '../../../services/tareas.service';
+import { Subscription, interval } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   standalone: true,
@@ -8,25 +10,49 @@ import { TareasService } from '../../../services/tareas.service';
   templateUrl: './users.component.html',
   styles: ``
 })
-export default class UsersComponent {
+export default class UsersComponent implements OnInit, OnDestroy {
   tareasForm!: FormGroup;
-  constructor(private fb: FormBuilder,private tareasService: TareasService) {
+  tareasSubscription: Subscription | undefined;
+
+  constructor(private fb: FormBuilder, private tareasService: TareasService) {
     this.tareasForm = this.fb.group({
       nombre_tarea: ['', [Validators.required, Validators.maxLength(100)]],
       descripcion_tarea: ['', Validators.required],
       fechainicio: ['', Validators.required],
-      fechafin: [''] // Nota: fechjaFin_tarea parece tener un error tipográfico, considera corregirlo a fechaFin_tarea tanto aquí como en el HTML.
+      fechafin: ['']
     });
+  }
 
-}
+  ngOnInit(): void {
+    // Empieza a observar los cambios en las tareas
+    this.startObservingTareasChanges();
+  }
+
+  ngOnDestroy(): void {
+    // Cancela la suscripción al destruir el componente para evitar memory leaks
+    if (this.tareasSubscription) {
+      this.tareasSubscription.unsubscribe();
+    }
+  }
+
   onSubmit(): void {
     if (this.tareasForm.valid) {
       console.log('Formulario de Tareas:', this.tareasForm.value);
       this.tareasService.insertarTarea(this.tareasForm);
       this.tareasForm.reset();
-      // Aquí iría la lógica para procesar los datos del formulario, como enviarlos a un backend.
     } else {
       console.log('Formulario no válido');
     }
+  }
+
+  private startObservingTareasChanges(): void {
+    // Crea una suscripción que se actualiza cada 5 segundos y verifica los cambios en las tareas
+    this.tareasSubscription = interval(5000).pipe(
+      switchMap(() => this.tareasService.obtenerTareas())
+    ).subscribe(tareas => {
+      console.log('Tareas actualizadas:', tareas);
+      // Aquí puedes realizar cualquier acción que necesites con las tareas actualizadas,
+      // como actualizar la vista del componente.
+    });
   }
 }
